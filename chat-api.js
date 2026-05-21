73c3f4de8c6178892ca912277a5889418d464cad
한지혜 <gkswlgp705@gmail.com>
2026-05-22 05:35:37 +0900

    Update chat-api.js

--- chat-api.js
+++ chat-api.js
@@ -1,9 +1,15 @@
-async function search(query) {
+async function ragSearch(query) {
   const res = await fetch("/api/search", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify({ query })
   });
+
+  if (!res.ok) {
+    throw new Error("API ERROR");
+  }
 
   return await res.json();
 }
