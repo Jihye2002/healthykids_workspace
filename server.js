e0bc00d57ec145f2a32c2958fd3eabd783abe3f5
한지혜 <gkswlgp705@gmail.com>
2026-05-24 01:25:22 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 const multer = require("multer");
@@ -20,14 +21,19 @@ let VECTOR_DB = [];
 /* =========================
    MIDDLEWARE
 ========================= */
-app.use(express.json());
+
+app.use(cors());
+
+app.use(express.json({
+  limit:"50mb"
+}));
 
 app.use(express.urlencoded({
-    extended:true
+  extended:true,
+  limit:"50mb"
 }));
 
 app.use(express.static(__dirname));
-
 /* =========================
    UPLOAD
 ========================= */
